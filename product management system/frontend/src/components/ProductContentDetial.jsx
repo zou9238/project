d08e5ec9c0befd34e1869edae804dd969f93cebf
zou9238 <145095192+zou9238@users.noreply.github.com>
2024-05-30 09:25:19 +0800

    Delete product management system directory

--- product management system/frontend/src/components/ProductContentDetial.jsx
+++
@@ -1,9 +0,0 @@
-export const ProductContentDetial = (props) => {
-
-    return (
-        <div  {...props.product.id}>
-            <p><strong>ProductContentDetial</strong> </p>
-            {props.product.name}
-        </div>
-    );
-}
